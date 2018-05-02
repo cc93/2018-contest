79f22ff54f571b8182265cc8e00c0780c76a854a
lichong <543281892@qq.com>
2018-05-02 18:55:22 +0800

    score addition animation ok

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import '../css/main.css'
-import {getRandomItem} from 'ccjs/util/util.js'
-import eventManager from 'ccjs/util/event-manager.js'
+import {getRandomItem} from 'ccjs/util'
+import eventManager from 'ccjs/event'
 import gameTemplate from '../templates/game.art'
 
 'use strict'
@@ -8,7 +8,7 @@ import gameTemplate from '../templates/game.art'
 const SETTINGS = {
     rowLen: 4,
     colLen: 4,
-    target: 16,
+    target: 24,
 }
 
 
@@ -92,17 +92,6 @@ class Tile {
         // flag isMerges
         this.status.merged = true
         // emit events
-        // let cbs = Tile.events['tileValueChanged']
-        // if (cbs !== undefined) {
-        //     cbs.forEach((cb) => {
-        //         cb({
-        //             type: 'tileValueChanged',
-        //             tile: this,
-        //             newValue: value,
-        //             oldValue: oldValue
-        //         })
-        //     })
-        // }
         eventManager.emit('tileValueChanged', {
             tile: this,
             newValue: value,
@@ -159,13 +148,31 @@ class Tile {
 
 
 class Game {
+    get bestScore() {
+        return this._bestScore
+    }
+    set bestScore(value) {
+        this._bestScore = value
+        this.bestScoreEl.innerText = value
+    }
     get score() {
         return this._score
     }
 
     set score(value) {
+        let oldValue = this._score,
+            addition = value - oldValue
         this._score = value
         this.scoreEl.innerText = value
+        let el = this.scoreAddEl
+        el.innerText = '+' + addition
+        if (addition > 0) {
+            // update className to trigger the animation
+            el.className = ''
+            setTimeout(() => {
+                el.className = 'game-score-addition'
+            })
+        }
     }
 
     constructor(el) {
@@ -176,59 +183,128 @@ class Game {
         Tile.tilesContainer = el.querySelector('.tiles-container')
         this.scoreEl = el.querySelector('.game-score')
         this._score = 0
+        this.scoreAddEl = el.querySelector('.game-score-addition')
+        this.bestScoreEl = el.querySelector('.game-best-score')
+        this._bestScore = 0
         this.btnNewGame = el.querySelector('.new-game')
         this.gameResultEl = el.querySelector('.game-result')
         this.btnRetry = el.querySelector('.game-retry')
         this.overlay = el.querySelector('.game-overlay')
+        this.running = false
+        this.bindEventsHandler()
 
-        // fix events context
-        this.gameEvents = this.gameEvents.bind(this)
-        document.addEventListener('click', this.gameEvents)
-        // self defined events
-        // Tile.addEventHandler('tileValueChanged', this.gameEvents)
-        eventManager.addListener('tileValueChanged', this.gameEvents)
     }
 
-    newGame() {
+    bindEventsHandler() {
+        // DOM events
+        DOMEventsHandler = DOMEventsHandler.bind(this)
+        document.addEventListener('click', DOMEventsHandler)
+        document.addEventListener('keydown', DOMEventsHandler)
+        document.addEventListener('touchstart', DOMEventsHandler)
+        document.addEventListener('touchmove', DOMEventsHandler, {passive: false})
+        document.addEventListener('touchend', DOMEventsHandler)
+        // self defined events
+        // fix events context
+        this.gameEventsHandler = this.gameEventsHandler.bind(this)
+        eventManager.addHandler('tileValueChanged', this.gameEventsHandler)
+        eventManager.addHandler('tileMove', this.gameEventsHandler)
+        let x0, y0
+        function DOMEventsHandler(ev) {
+            switch (ev.type) {
+                case 'click': {
+                    if (ev.target === this.btnNewGame ||
+                        ev.target === this.btnRetry) {
+                        this.stopGame()
+                        this.startGame()
+                        // eventManager.emit('gameStart')
+                    }
+                    break
+                }
+                case 'keydown': {
+                    if(ev.ctrlKey||ev.altKey||ev.metaKey||ev.shiftKey) break
+                    const keyMap = {
+                        37: 'left',
+                        38: 'up',
+                        39: 'right',
+                        40: 'down'
+                    }
+                    let direction = keyMap[ev.keyCode]
+                    if (direction !== undefined) {
+                        ev.preventDefault()
+                        eventManager.emit('tileMove', {direction})
+                    }
+                    break
+                }
+                case 'touchstart': {
+                    x0 = ev.touches[0].clientX
+                    y0 = ev.touches[0].clientY
+                    break
+                }
+                case 'touchmove': {
+                    // prevent default behaviour: scroll page
+                    ev.preventDefault()
+                    break
+                }
+                case 'touchend': {
+                    // Ignore if still touching with one or more fingers
+                    if (x0===undefined || y0===undefined || ev.touches.length > 0) break
+                    let x1 = ev.changedTouches[0].clientX,
+                        y1 = ev.changedTouches[0].clientY,
+                        dx = x1 - x0,
+                        dy = y1 - y0,
+                        adx = Math.abs(dx),
+                        ady = Math.abs(dy),
+                        dire = ''
+                    if (adx > 10 || ady > 10) {
+                        if (adx > ady) {
+                            dire = dx > 0? 'right': 'left'
+                        } else {
+                            dire = dy > 0? 'down':'up'
+                        }
+                        eventManager.emit('tileMove', {direction: dire})
+                    }
+                    break
+                }
+            }
+        }
+
+
+    }
+
+    startGame() {
         this.grids.clearContents()
         Tile.tilesContainer.innerHTML = ''
         this.overlay.style.display = 'none'
         this.score = 0
-        document.addEventListener('keydown', this.gameEvents)
+        this.running = true
         // create 2 initial tiles
         this.newRandomTiles(2)
     }
 
-    gameEvents(ev) {
+    stopGame() {
+        this.running = false
+        // record best score
+        if(this.score > this.bestScore) {
+            this.bestScore = this.score
+        }
+    }
+
+    gameEventsHandler(ev) {
         switch (ev.type) {
-            case 'click': {
-                if (ev.target === this.btnNewGame ||
-                    ev.target === this.btnRetry) {
-                    this.newGame()
-                }
-                break
-            }
-            case 'keydown': {
-                const keyMap = {
-                    '37': 'left',
-                    '38': 'up',
-                    '39': 'right',
-                    '40': 'down'
-                }
-                let direction = keyMap[ev.keyCode + '']
-                if (direction !== undefined) {
-                    ev.preventDefault()
-                    this.resetTilesStatus()
-                    this.moveTiles(direction)
-                    this.checkTilesStatus()
-                }
-                break
-            }
             case 'tileValueChanged': {
                 // check if win
-                if (ev.newValue === SETTINGS.target) {
+                if (ev.newValue >= SETTINGS.target) {
                     this.setGameResult('win')
+                    this.stopGame()
                 }
+                break
+            }
+            case 'tileMove': {
+                if (!this.running) break
+                this.resetTilesStatus()
+                this.moveTiles(ev.direction)
+                this.checkTilesStatus()
+                break
             }
         }
     }
@@ -258,6 +334,7 @@ class Game {
             if (emptyPos.length === 0 && !this.hasValueEqualedAdjTiles()) {
                 // alert('Game over!')
                 this.setGameResult('lose')
+                this.stopGame()
             }
         }
     }
@@ -266,7 +343,6 @@ class Game {
         this.gameResultEl.className = `game-result ${result}`
         this.gameResultEl.innerHTML = result === 'win' ? 'You win!' : 'Game over!'
         this.overlay.style.display = 'block'
-        document.removeEventListener('keydown', this.gameEvents)
     }
 
     hasValueEqualedAdjTiles() {
@@ -295,6 +371,7 @@ class Game {
     moveTiles(direction) {
         // some game rules to be considered:
         // if none moved, then new tile will not generate
+        const ret = {scoreAddition:0}
         switch (direction) {
             case 'left': {
                 // colIndex--
@@ -311,7 +388,7 @@ class Game {
                             if (preTile !== null) {
                                 if (preTile.value === tile.value && merged === false) {
                                     preTile.value += tile.value
-                                    this.score += preTile.value
+                                    ret.scoreAddition += preTile.value
                                     tile.element.parentNode.removeChild(tile.element)
                                     this.grids.contents[ri][ci] = null
                                     preTile.status.merged = true
@@ -353,7 +430,7 @@ class Game {
                             if (preTile !== null) {
                                 if (preTile.value === tile.value && merged === false) {
                                     preTile.value += tile.value
-                                    this.score += preTile.value
+                                    ret.scoreAddition += preTile.value
                                     tile.element.parentNode.removeChild(tile.element)
                                     this.grids.contents[ri][ci] = null
                                     preTile.status.merged = true
@@ -385,7 +462,7 @@ class Game {
                 // rowIndex--
                 // every column allow one merged
                 let merged = []
-                for (let ci=0,len=this.grids.colLen; ci<len;ci++) {
+                for (let ci = 0, len = this.grids.colLen; ci < len; ci++) {
                     merged[ci] = false
                 }
                 for (let ri = 1, len = this.grids.contents.length; ri < len; ri++) {
@@ -400,7 +477,7 @@ class Game {
                                     // merged tile into the preTile with the same value
                                     // update preTile value
                                     preTile.value += tile.value
-                                    this.score += preTile.value
+                                    ret.scoreAddition += preTile.value
                                     // remove this tile
                                     tile.element.parentNode.removeChild(tile.element)
                                     this.grids.contents[ri][ci] = null
@@ -432,7 +509,7 @@ class Game {
                 // rowIndex++
                 // every column allow one merged
                 let merged = []
-                for (let ci=0,len=this.grids.colLen; ci<len;ci++) {
+                for (let ci = 0, len = this.grids.colLen; ci < len; ci++) {
                     merged[ci] = false
                 }
                 for (let ri = this.grids.contents.length - 2; ri >= 0; ri--) {
@@ -447,7 +524,7 @@ class Game {
                                     // merged tile into the preTile with the same value
                                     // update preTile value
                                     preTile.value += tile.value
-                                    this.score += preTile.value
+                                    ret.scoreAddition += preTile.value
                                     // remove this tile
                                     tile.element.parentNode.removeChild(tile.element)
                                     this.grids.contents[ri][ci] = null
@@ -478,6 +555,8 @@ class Game {
             default:
                 break
         }
+        this.score += ret.scoreAddition
+        return ret
     }
 
     newRandomTiles(n = 1, randomValues = [2, 4]) {
@@ -505,5 +584,5 @@ main
  */
 
 const game = new Game(document.querySelector('#gameApp'))
-game.newGame()
+game.startGame()
 
